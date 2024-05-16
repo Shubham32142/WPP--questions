7ca2dfa9acd28b9897a6810a9b5f186a2d92c22e
Shubham32142 <shubhamdubey23042004@gmail.com>
2024-05-16 18:48:20 +0530

    "wpp problem 4"

---
+++ wpp-problem4.js
@@ -0,0 +1,12 @@
+function daysTillChristmas() {
+    const date = new Date();
+    const christmasDate = new Date(date.getFullYear(), 11, 25);
+    if (christmasDate < date) {
+        christmasDate = new Date(date.getFullYear() + 1, 11, 25);
+    }
+    let differenceInTime = christmasDate.getTime() - date.getTime();
+    let daysUntil = Math.ceil(differenceInTime / (1000 * 3600 * 24))
+    return daysUntil;
+}
+
+console.log(daysTillChristmas());

fbca0ee428efbcbc498bdde976ad5ffc423c0824
Shubham32142 <shubhamdubey23042004@gmail.com>
2024-05-23 11:05:36 +0530

    "problem6"

---
+++ wpp-problem6.js
@@ -0,0 +1,6 @@
+// Write a JavaScript program to check whether a given positive number is a multiple of 3 or 7.
+function check(a) {
+    return a % 3 === 0 || a % 7 === 0
+
+}
+console.log(check(49));

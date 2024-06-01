5fb6634b72d6faa6ce554a5968ad53298e4ad0ee
Shubham32142 <shubhamdubey23042004@gmail.com>
2024-06-01 21:10:24 +0530

    //added wpp solution 8

---
+++ wpp-problem8.js
@@ -0,0 +1,5 @@
+//Write a JavaScript program to check whether a string starts with 'Java'  if it does not then  return False.
+function checkName(str) {
+    return str.startsWith('Java');
+}
+console.log(checkName('Javascript'));
